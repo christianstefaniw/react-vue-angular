f6da586fed4fb897dc9a40ea1ccd6b37c8e51e30
ChristianStefaniw <christian.stefaniw@student.tdsb.on.ca>
2020-12-11 13:31:17 -0500

    react calculator

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -1,25 +1,70 @@
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import ResultComponent from "./components/ResultComponent";
+import KeypadComponent from "./components/KeypadComponent";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+  constructor() {
+    super();
+
+    this.state = {
+      result: ""
+    }
+  }
+
+  onClick = button => {
+    if (button === "="){
+      this.calculate();
+    }
+    else if (button === "C"){
+      this.reset();
+    }
+    else if (button === "CE"){
+      this.backspace();
+    }
+    else {
+      this.setState({
+        result: this.state.result + button
+      })
+    }
+  }
+
+  calculate = () => {
+    try{
+      this.setState({
+        result: (eval(this.state.result) || "") + ""
+          }
+      );
+    } catch (e){
+      this.setState({
+        result: ""
+      });
+    }
+  };
+
+  reset = () => {
+    this.setState({
+      result: ""
+    });
+  };
+
+  backspace = () => {
+    this.setState({
+      result: this.state.result.slice(0, -1)
+    });
+  };
+
+  render() {
+    return (
+        <div>
+          <div className="calculator-body">
+            <h1>My React Calculator</h1>
+            <ResultComponent result={this.state.result}/>
+            <KeypadComponent onClick={this.onClick}/>
+          </div>
+        </div>
+    )
+  }
 }
 
-export default App;
+export default App
